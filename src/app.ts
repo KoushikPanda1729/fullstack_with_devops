af8810144eab85305f93ac8d148f4afd87c9945f
Git_Koushik <programmer747767@gmail.com>
2024-11-23 13:31:14 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import logger from "./config/logger";
+import { HttpError } from "http-errors";
 
 const app = express();
 app.get("/", (req, res) => {
   res.status(200).json({
     name: "Koushik",
     dept: "MCA",
+  });
+});
+
+// global error handler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+  res.status(err.statusCode).json({
+    type: err.name,
+    message: err.message,
+    location: "",
+    path: "",
   });
 });
 export default app;
